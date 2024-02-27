c4e501b5cece93bee267a506aa3d9fde63b6dc80
Maksim Khitrov <Khitrov.Maks@gmail.com>
2024-02-27 13:29:36 +0300

    Added exceptions.

--- apps/reproduces-http-errors/src/reproduces-http-errors.controller.ts
+++ apps/reproduces-http-errors/src/reproduces-http-errors.controller.ts
@@ -1,23 +1,138 @@
-import { Controller, Get, HttpCode, HttpStatus, InternalServerErrorException } from '@nestjs/common';
-import { ReproducesHttpErrorsService } from './reproduces-http-errors.service';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+  ForbiddenException,
+  NotAcceptableException,
+  RequestTimeoutException,
+  ConflictException,
+  GoneException,
+  HttpVersionNotSupportedException,
+  PayloadTooLargeException,
+  UnsupportedMediaTypeException,
+  UnprocessableEntityException,
+  InternalServerErrorException,
+  NotImplementedException,
+  ImATeapotException,
+  MethodNotAllowedException,
+  BadGatewayException,
+  ServiceUnavailableException,
+  GatewayTimeoutException,
+  PreconditionFailedException,
+} from '@nestjs/common';
 
 @Controller('http-errors')
 export class ReproducesHttpErrorsController {
   constructor(
-    private readonly reproducesHttpErrorsService: ReproducesHttpErrorsService,
   ) {}
-
-  @Get('health')
-  health(): boolean {
-    return this.reproducesHttpErrorsService.isActive();
-  }
-
-  @Get('internal-server-error')
-  async eTimedOut(): Promise<string> {
-    throw new InternalServerErrorException();
-  }
 
   @Get('200')
   @HttpCode(HttpStatus.OK)
-  ok(): void {}
+  ok(): string {
+    return 'Ok';
+  }
+
+  @Get('400')
+  badRequestException(): void {
+    throw new BadRequestException();
+  }
+
+  @Get('401')
+  unauthorizedException(): void {
+    throw new UnauthorizedException();
+  }
+
+  @Get('403')
+  forbiddenException(): void {
+    throw new ForbiddenException();
+  }
+
+  @Get('404')
+  notFound(): void {
+    throw new NotFoundException();
+  }
+
+  @Get('405')
+  async methodNotAllowedException(): Promise<string> {
+    throw new MethodNotAllowedException();
+  }
+
+  @Get('406')
+  notAcceptableException(): void {
+    throw new NotAcceptableException();
+  }
+
+  @Get('408')
+  requestTimeoutException(): void {
+    throw new RequestTimeoutException();
+  }
+
+  @Get('409')
+  conflictException(): void {
+    throw new ConflictException();
+  }
+
+  @Get('410')
+  goneException(): void {
+    throw new GoneException();
+  }
+
+  @Get('412')
+  async preconditionFailedException(): Promise<string> {
+    throw new PreconditionFailedException();
+  }
+
+  @Get('413')
+  payloadTooLargeException(): void {
+    throw new PayloadTooLargeException();
+  }
+
+  @Get('415')
+  unsupportedMediaTypeException(): void {
+    throw new UnsupportedMediaTypeException();
+  }
+
+  @Get('418')
+  async imATeapotException(): Promise<string> {
+    throw new ImATeapotException();
+  }
+
+  @Get('422')
+  unprocessableEntityException(): void {
+    throw new UnprocessableEntityException();
+  }
+
+  @Get('500')
+  async internalServerErrorException(): Promise<string> {
+    throw new InternalServerErrorException();
+  }
+
+  @Get('501')
+  async notImplementedException(): Promise<string> {
+    throw new NotImplementedException();
+  }
+
+  @Get('502')
+  async badGatewayException(): Promise<string> {
+    throw new BadGatewayException();
+  }
+
+  @Get('503')
+  async serviceUnavailableException(): Promise<string> {
+    throw new ServiceUnavailableException();
+  }
+
+  @Get('504')
+  async gatewayTimeoutException(): Promise<string> {
+    throw new GatewayTimeoutException();
+  }
+
+  @Get('505')
+  httpVersionNotSupportedException(): void {
+    throw new HttpVersionNotSupportedException();
+  }
 }
--- apps/reproduces-http-errors/src/reproduces-http-errors.module.ts
+++ apps/reproduces-http-errors/src/reproduces-http-errors.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ReproducesHttpErrorsController } from './reproduces-http-errors.controller';
-import { ReproducesHttpErrorsService } from './reproduces-http-errors.service';
 
 @Module({
   imports: [],
@@ -8,7 +7,6 @@ import { ReproducesHttpErrorsService } from './reproduces-http-errors.service';
     ReproducesHttpErrorsController,
   ],
   providers: [
-    ReproducesHttpErrorsService,
   ],
 })
 export class ReproducesHttpErrorsModule {
